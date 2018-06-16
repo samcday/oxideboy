b5282f84b90ddadfc61d24e9087bdda59f478d74
Sam Day <me@samcday.com>
2018-06-16 17:43:11 +0200

    Run dev emulation in seperate thread

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -10,6 +10,8 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::time::Duration;
+use std::thread;
+use std::sync::{Mutex, Arc};
 
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
@@ -21,8 +23,6 @@ fn main() -> io::Result<()> {
     let mut f = File::open(&args[1])?;
     let mut rom = Vec::new();
     f.read_to_end(&mut rom)?;
-    let mut cart = gameboy::cartridges::MBC1Cart::new(&rom);
-    let cpu = &mut lr35902::CPU::new(&mut cart);
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -43,8 +43,40 @@ fn main() -> io::Result<()> {
         .interval(Duration::new(0, 16660000)) //add quantum tokens every 1 second
         .build();
 
+    // let arr: [u8; 160*144] = ;
+    let framebuffer = Arc::new(Mutex::new([0 as u32; 160*144]));
+
+    let emu_framebuffer = Arc::clone(&framebuffer);
+    thread::spawn(move || {
+        let mut cart = gameboy::cartridges::MBC1Cart::new(&rom);
+        let cpu = &mut lr35902::CPU::new(&mut cart);
+
+        loop {
+            if cpu.pc() == 0x681 {
+                break;
+            }
+
+            cpu.run();
+            let ser = cpu.serial_get();
+            if ser.is_some() {
+              io::stdout().write(&[ser.unwrap()]).unwrap();
+              io::stdout().flush().unwrap();
+            }
+            if cpu.is_vblank() {
+                {
+                    let mut emu_framebuffer = emu_framebuffer.lock().unwrap();
+                    emu_framebuffer.copy_from_slice(&cpu.framebuffer()[..]);
+                }
+            }
+        }
+
+        println!();
+        println!("Executed a total of {} clock cycles", cpu.cycle_count);
+        println!();
+    });
+
     'running: loop {
-        // ratelimit.wait();
+        ratelimit.wait();
 
         for event in event_pump.poll_iter() {
             match event {
@@ -56,41 +88,24 @@ fn main() -> io::Result<()> {
             }
         }
 
-        loop {
-            if cpu.pc() == 0x681 {
-                break 'running;
-            }
-
-            cpu.run();
-            let ser = cpu.serial_get();
-            if ser.is_some() {
-              io::stdout().write(&[ser.unwrap()]).unwrap();
-              io::stdout().flush().unwrap();
-            }
-            if cpu.is_vblank() {
-                break;
-            }
+        {
+            let framebuffer = framebuffer.lock().unwrap();
+            texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                for y in 0..144 {
+                    for x in 0..160 {
+                        let offset = y*pitch + (x*3);
+                        buffer[offset] = framebuffer[y*160+x] as u8;
+                        buffer[offset + 1] = framebuffer[y*160+x] as u8;
+                        buffer[offset + 2] = framebuffer[y*160+x] as u8;
+                    }
+                }
+            }).unwrap();
         }
 
-        let framebuffer = cpu.framebuffer();
-        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-            for y in 0..144 {
-                for x in 0..160 {
-                    let offset = y*pitch + (x*3);
-                    buffer[offset] = framebuffer[y*160+x] as u8;
-                    buffer[offset + 1] = framebuffer[y*160+x] as u8;
-                    buffer[offset + 2] = framebuffer[y*160+x] as u8;
-                }
-            }
-        }).unwrap();
         canvas.clear();
         canvas.copy(&texture, None, Some(Rect::new(0, 0, 320, 288))).unwrap();
         canvas.present();
     }
-
-    println!();
-    println!("Executed a total of {} clock cycles", cpu.cycle_count);
-    println!();
 
     Ok(())
 }
