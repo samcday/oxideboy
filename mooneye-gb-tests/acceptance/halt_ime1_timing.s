8373acadf89b61c56afc7c3b9e75e6403b974b50
Sam Day <me@samcday.com>
2018-06-22 13:02:19 +0200

    Check in mooneye-gb/tests @ a4b4acce073c86c4e692f1a59305e611221b91cd

---
+++ mooneye-gb-tests/acceptance/halt_ime1_timing.s
@@ -0,0 +1,62 @@
+; Copyright (C) 2014-2018 Joonas Javanainen <joonas.javanainen@gmail.com>
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+
+; If IME=1, HALT is expected to immediately service an interrupt.
+; So, if the interrupt service routine doesn't return,
+; the instruction after HALT should never get executed
+
+; Verified results:
+;   pass: DMG, MGB, SGB, SGB2, CGB, AGB, AGS
+;   fail: -
+
+.incdir "../common"
+.include "common.s"
+
+  ei
+
+  ; B = 0
+  xor a
+  ld b, a
+
+  ; Enable timer interrupt
+  ld a, INTR_TIMER
+  ldh (<IE), a
+
+  ; TIMA = $F0
+  ld a, $F0
+  ldh (<TIMA), a
+
+  ; Start timer at 262144 Hz
+  ld a, $05
+  ldh (<TAC), a
+
+  halt
+  ; This should never get executed
+  inc b
+
+  test_failure
+
+test_finish:
+  save_results
+  assert_b $00
+  jp process_results
+
+.org INTR_VEC_TIMER
+  jp test_finish
