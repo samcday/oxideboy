2c93fbce359d1a33ffda48b048591f110a3c78c9
Sam Day <me@samcday.com>
2018-08-01 12:37:51 +0200

    Color all mismatching pixels in PPU acceptance tests

--- gameboy/tests/common.rs
+++ gameboy/tests/common.rs
@@ -8,6 +8,10 @@ pub fn compare_framebuffer<F>(framebuf: &[u32], expected: &[u8], color_conv: F)
     let expected_img = image::load_from_memory(expected).unwrap();
     let expected_img = expected_img.to_rgb();
 
+    let mut output_image = vec![0; framebuf.len()*3];
+
+    let mut image_equal = true;
+
     for y in 0..144 {
         for x in 0..160 {
             let expected_rgb = expected_img.get_pixel(x as u32, y as u32);
@@ -19,25 +23,24 @@ pub fn compare_framebuffer<F>(framebuf: &[u32], expected: &[u8], color_conv: F)
             let expected_rgb = color_conv(expected_rgb);
 
             if framebuffer_pix != expected_rgb {
-                // Save the framebuffer to a temp file for examination.
-                let mut path = env::temp_dir();
-                path.push("result.png");
-                let mut framebuf_image = vec![0u8; 160*144*3];
-                for i in 0..160*144 {
-                    if i == ((y * 160) + x) {
-                        framebuf_image[i*3] = 0xFF;
-                        framebuf_image[i*3+1] = 0;
-                        framebuf_image[i*3+2] = 0;
-                    } else {
-                        framebuf_image[i*3] = ((framebuf[i] & 0xFF0000) >> 16) as u8;
-                        framebuf_image[i*3+1] = ((framebuf[i] & 0xFF00) >> 8) as u8;
-                        framebuf_image[i*3+2] = (framebuf[i] & 0xFF) as u8;
-                    }
-                }
-                image::save_buffer(path.clone(), &framebuf_image[..], 160, 144, image::ColorType::RGB(8)).unwrap();
-
-                panic!("Pixel {},{} does not match. Expected {:X}, got {:X}. Result saved to {}", x, y, expected_rgb, framebuffer_pix, path.to_str().unwrap());
+                image_equal = false;
+                // Mark this pixel red to show up the differences.
+                output_image[((y * 160) + x) * 3] = 0xFF;
+            } else {
+                output_image[((y * 160) + x) * 3] = ((framebuffer_pix & 0xFF0000) >> 16) as u8;
+                output_image[((y * 160) + x) * 3 + 1] = ((framebuffer_pix & 0xFF00) >> 8) as u8;
+                output_image[((y * 160) + x) * 3 + 2] = (framebuffer_pix & 0xFF) as u8;
             }
+
         }
+    }
+
+    if !image_equal {
+        // Save the framebuffer to a temp file for examination.
+        let mut path = env::temp_dir();
+        path.push("result.png");
+        image::save_buffer(path.clone(), &output_image[..], 160, 144, image::ColorType::RGB(8)).unwrap();
+
+        panic!("Images do not match. Result saved to {}", path.to_str().unwrap());
     }
 }
