d74e25cc6800baa30365e381fead2519b127786b
Sam Day <me@samcday.com>
2019-02-21 12:06:48 +0100

    Okay neat - all blargg CPU tests are passing

--- dev-frontend/src/main.rs
+++ dev-frontend/src/main.rs
@@ -14,9 +14,6 @@ fn main() -> Result<()> {
 	let hw = gameboy::Gameboy::new(gameboy::Model::DMG0, rom);
 	let mut cpu = gameboy::cpu::Cpu::new(hw);
 
-	cpu.hw.ppu.test = 321;
-	println!("Hmm: {}", cpu.hw.ppu.test);
-
 	cpu.fetch_decode_execute();
 
 	Ok(())
--- gameboy/tests/blargg.rs
+++ gameboy/tests/blargg.rs
@@ -11,8 +11,8 @@ fn run_blargg_serial_test(rom: &[u8]) {
 
     let start = Instant::now();
     loop {
-        if start.elapsed().as_secs() > 1 {
-            panic!("Test ran for more than 1 second. PC={}, Output: {}", cpu.pc, serial_output);
+        if start.elapsed().as_secs() > 5 {
+            panic!("Test ran for more than 5 seconds. PC={}, Output: {}", cpu.pc, serial_output);
         }
 
     	cpu.fetch_decode_execute();
@@ -30,4 +30,14 @@ fn run_blargg_serial_test(rom: &[u8]) {
     }
 }
 
+#[test] fn blargg_cpu_instrs_01() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/01-special.gb")); }
+#[test] fn blargg_cpu_instrs_02() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/02-interrupts.gb")); }
+#[test] fn blargg_cpu_instrs_03() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/03-op sp,hl.gb")); }
+#[test] fn blargg_cpu_instrs_04() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/04-op r,imm.gb")); }
+#[test] fn blargg_cpu_instrs_05() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/05-op rp.gb")); }
 #[test] fn blargg_cpu_instrs_06() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/06-ld r,r.gb")); }
+#[test] fn blargg_cpu_instrs_07() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/07-jr,jp,call,ret,rst.gb")); }
+#[test] fn blargg_cpu_instrs_08() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/08-misc instrs.gb")); }
+#[test] fn blargg_cpu_instrs_09() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/09-op r,r.gb")); }
+#[test] fn blargg_cpu_instrs_10() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/10-bit ops.gb")); }
+#[test] fn blargg_cpu_instrs_11() { run_blargg_serial_test(include_bytes!("blargg/cpu_instrs/11-op a,(hl).gb")); }
