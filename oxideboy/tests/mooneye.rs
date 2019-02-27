945d92c328e7091d903649aa0d15dc25412b3e46
Sam Day <me@samcday.com>
2019-02-27 15:25:20 +0100

    core: paste crate is the bomb, mooneye test suite is much cleaner now. Also I somehow fixed the halt_ime0_ei test at some point ...

--- oxideboy/tests/mooneye.rs
+++ oxideboy/tests/mooneye.rs
@@ -1,4 +1,5 @@
 use oxideboy::*;
+use paste;
 use std::time::Instant;
 
 struct DebugBreakpointListener {
@@ -46,83 +47,85 @@ fn run_mooneye_test(rom: &[u8], model: Model, enable_bootrom: bool) {
 macro_rules! test_cases {
     ( $( $name:ident: $path:tt,$model:expr,$bootrom:expr, )* ) => {
         $(
-        #[test]
-        fn $name () {
-            run_mooneye_test(include_bytes!(concat!("mooneye/", $path, ".gb")), $model, $bootrom);
+        paste::item! {
+            #[test]
+            fn [<mooneye_acceptance_ $name>] () {
+                run_mooneye_test(include_bytes!(concat!("mooneye/acceptance/", $path, ".gb")), $model, $bootrom);
+            }
         }
         )*
     }
 }
 
 test_cases! {
-    mooneye_acceptance_bits_mem_oam: "acceptance/bits/mem_oam", Model::DMG, false,
-    mooneye_acceptance_bits_reg_f: "acceptance/bits/reg_f", Model::DMG, false,
-    mooneye_acceptance_bits_unused_hwio_gs: "acceptance/bits/unused_hwio-GS", Model::DMG, false,
+    bits_mem_oam:                       "bits/mem_oam",                     Model::DMG, false,
+    bits_reg_f:                         "bits/reg_f",                       Model::DMG, false,
+    bits_unused_hwio_gs:                "bits/unused_hwio-GS",              Model::DMG, false,
 
-    mooneye_acceptance_instr_daa: "acceptance/instr/daa", Model::DMG, false,
+    instr_daa:                          "instr/daa",                        Model::DMG, false,
 
-    mooneye_acceptance_interrupts_ie_push: "acceptance/interrupts/ie_push", Model::DMG, false,
+    interrupts_ie_push:                 "interrupts/ie_push",               Model::DMG, false,
 
-    mooneye_acceptance_oam_dma_basic: "acceptance/oam_dma/basic", Model::DMG, false,
-    mooneye_acceptance_oam_dma_reg_read: "acceptance/oam_dma/reg_read", Model::DMG, false,
+    oam_dma_basic:                      "oam_dma/basic",                    Model::DMG, false,
+    oam_dma_reg_read:                   "oam_dma/reg_read",                 Model::DMG, false,
 
-    // mooneye_acceptance_ppu_hblank_ly_scx_timing_gs: "acceptance/ppu/hblank_ly_scx_timing-GS", Model::DMG, false,
-    mooneye_acceptance_ppu_intr_1_2_timing_gs: "acceptance/ppu/intr_1_2_timing-GS", Model::DMG, false,
-    mooneye_acceptance_ppu_intr_2_0_timing: "acceptance/ppu/intr_2_0_timing", Model::DMG, false,
-    mooneye_acceptance_ppu_intr_2_mode0_timing: "acceptance/ppu/intr_2_mode0_timing", Model::DMG, false,
-    // mooneye_acceptance_ppu_intr_2_mode0_timing_sprites: "acceptance/ppu/intr_2_mode0_timing_sprites", Model::DMG, false,
-    mooneye_acceptance_ppu_intr_2_mode3_timing: "acceptance/ppu/intr_2_mode3_timing", Model::DMG, false,
-    mooneye_acceptance_ppu_intr_2_oam_ok_timing: "acceptance/ppu/intr_2_oam_ok_timing", Model::DMG, false,
-    // mooneye_acceptance_ppu_lcdon_write_timing_gs: "acceptance/ppu/lcdon_write_timing-GS", Model::DMG, false,
-    // mooneye_acceptance_ppu_stat_irq_blocking: "acceptance/ppu/stat_irq_blocking", Model::DMG, false,
-    // mooneye_acceptance_ppu_vblank_stat_intr_gs: "acceptance/ppu/vblank_stat_intr-GS", Model::DMG, false,
+//  ppu_hblank_ly_scx_timing_gs:        "ppu/hblank_ly_scx_timing-GS",      Model::DMG, false,
+    ppu_intr_1_2_timing_gs:             "ppu/intr_1_2_timing-GS",           Model::DMG, false,
+    ppu_intr_2_0_timing:                "ppu/intr_2_0_timing",              Model::DMG, false,
+    ppu_intr_2_mode0_timing:            "ppu/intr_2_mode0_timing",          Model::DMG, false,
+//  ppu_intr_2_mode0_timing_sprites:    "ppu/intr_2_mode0_timing_sprites",  Model::DMG, false,
+    ppu_intr_2_mode3_timing:            "ppu/intr_2_mode3_timing",          Model::DMG, false,
+    ppu_intr_2_oam_ok_timing:           "ppu/intr_2_oam_ok_timing",         Model::DMG, false,
+//  ppu_lcdon_write_timing_gs:          "ppu/lcdon_write_timing-GS",        Model::DMG, false,
+//  ppu_stat_irq_blocking:              "ppu/stat_irq_blocking",            Model::DMG, false,
+//  ppu_vblank_stat_intr_gs:            "ppu/vblank_stat_intr-GS",          Model::DMG, false,
 
-    mooneye_acceptance_timer_div_write: "acceptance/timer/div_write", Model::DMG, false,
-    mooneye_acceptance_timer_rapid_toggle: "acceptance/timer/rapid_toggle", Model::DMG, false,
-    mooneye_acceptance_timer_tim00: "acceptance/timer/tim00", Model::DMG, false,
-    mooneye_acceptance_timer_tim00_div_trigger: "acceptance/timer/tim00_div_trigger", Model::DMG, false,
-    mooneye_acceptance_timer_tim01: "acceptance/timer/tim01", Model::DMG, false,
-    mooneye_acceptance_timer_tim01_div_trigger: "acceptance/timer/tim01_div_trigger", Model::DMG, false,
-    mooneye_acceptance_timer_tim10: "acceptance/timer/tim10", Model::DMG, false,
-    mooneye_acceptance_timer_tim10_div_trigger: "acceptance/timer/tim10_div_trigger", Model::DMG, false,
-    mooneye_acceptance_timer_tim11: "acceptance/timer/tim11", Model::DMG, false,
-    mooneye_acceptance_timer_tim11_div_trigger: "acceptance/timer/tim11_div_trigger", Model::DMG, false,
-    mooneye_acceptance_timer_tima_reload: "acceptance/timer/tima_reload", Model::DMG, false,
-    mooneye_acceptance_timer_tima_write_reloading: "acceptance/timer/tima_write_reloading", Model::DMG, false,
-    mooneye_acceptance_timer_tma_write_reloading: "acceptance/timer/tma_write_reloading", Model::DMG, false,
+    timer_div_write:                    "timer/div_write",                  Model::DMG, false,
+    timer_rapid_toggle:                 "timer/rapid_toggle",               Model::DMG, false,
+    timer_tim00:                        "timer/tim00",                      Model::DMG, false,
+    timer_tim00_div_trigger:            "timer/tim00_div_trigger",          Model::DMG, false,
+    timer_tim01:                        "timer/tim01",                      Model::DMG, false,
+    timer_tim01_div_trigger:            "timer/tim01_div_trigger",          Model::DMG, false,
+    timer_tim10:                        "timer/tim10",                      Model::DMG, false,
+    timer_tim10_div_trigger:            "timer/tim10_div_trigger",          Model::DMG, false,
+    timer_tim11:                        "timer/tim11",                      Model::DMG, false,
+    timer_tim11_div_trigger:            "timer/tim11_div_trigger",          Model::DMG, false,
+    timer_tima_reload:                  "timer/tima_reload",                Model::DMG, false,
+    timer_tima_write_reloading:         "timer/tima_write_reloading",       Model::DMG, false,
+    timer_tma_write_reloading:          "timer/tma_write_reloading",        Model::DMG, false,
 
-    mooneye_acceptance_add_sp_e_timing: "acceptance/add_sp_e_timing", Model::DMG, false,
-    // mooneye_acceptance_boot_div_dmg0: "acceptance/boot_div-dmg0", Model::DMG0, false,
-    mooneye_acceptance_boot_hwio_dmg0: "acceptance/boot_hwio-dmg0", Model::DMG0, false,
-    // mooneye_acceptance_boot_hwio_dmg0_realbootrom: "acceptance/boot_hwio-dmg0", Model::DMG0, true,
-    mooneye_acceptance_boot_regs_dmg0: "acceptance/boot_regs-dmg0", Model::DMG0, false,
-    mooneye_acceptance_boot_regs_dmg0_realbootrom: "acceptance/boot_regs-dmg0", Model::DMG0, true,
-    mooneye_acceptance_call_cc_timing: "acceptance/call_cc_timing", Model::DMG, false,
-    mooneye_acceptance_call_cc_timing2: "acceptance/call_cc_timing2", Model::DMG, false,
-    mooneye_acceptance_call_timing: "acceptance/call_timing", Model::DMG, false,
-    mooneye_acceptance_call_timing2: "acceptance/call_timing2", Model::DMG, false,
-    mooneye_acceptance_div_timing: "acceptance/div_timing", Model::DMG, false,
-    mooneye_acceptance_ei_sequence: "acceptance/ei_sequence", Model::DMG, false,
-    mooneye_acceptance_ei_timing: "acceptance/ei_timing", Model::DMG, false,
-    // mooneye_acceptance_halt_ime0_ei: "acceptance/halt_ime0_ei", Model::DMG, false,
-    mooneye_acceptance_halt_ime0_nointr_timing: "acceptance/halt_ime0_nointr_timing", Model::DMG, false,
-    mooneye_acceptance_halt_ime1_timing: "acceptance/halt_ime1_timing", Model::DMG, false,
-    mooneye_acceptance_if_ie_registers: "acceptance/if_ie_registers", Model::DMG, false,
-    mooneye_acceptance_intr_timing: "acceptance/intr_timing", Model::DMG, false,
-    mooneye_acceptance_jp_cc_timing: "acceptance/jp_cc_timing", Model::DMG, false,
-    mooneye_acceptance_jp_timing: "acceptance/jp_timing", Model::DMG, false,
-    mooneye_acceptance_ld_hl_sp_e_timing: "acceptance/ld_hl_sp_e_timing", Model::DMG, false,
-    mooneye_acceptance_oam_dma_restart: "acceptance/oam_dma_restart", Model::DMG, false,
-    mooneye_acceptance_oam_dma_start: "acceptance/oam_dma_start", Model::DMG, false,
-    mooneye_acceptance_oam_dma_timing: "acceptance/oam_dma_timing", Model::DMG, false,
-    mooneye_acceptance_pop_timing: "acceptance/pop_timing", Model::DMG, false,
-    mooneye_acceptance_push_timing: "acceptance/push_timing", Model::DMG, false,
-    mooneye_acceptance_rapid_di_ei: "acceptance/rapid_di_ei", Model::DMG, false,
-    mooneye_acceptance_ret_cc_timing: "acceptance/ret_cc_timing", Model::DMG, false,
-    mooneye_acceptance_ret_timing: "acceptance/ret_timing", Model::DMG, false,
-    mooneye_acceptance_reti_intr_timing: "acceptance/reti_intr_timing", Model::DMG, false,
-    mooneye_acceptance_reti_timing: "acceptance/reti_timing", Model::DMG, false,
-    mooneye_acceptance_rst_timing: "acceptance/rst_timing", Model::DMG, false,
+    add_sp_e_timing:                    "add_sp_e_timing",                  Model::DMG, false,
+//  boot_div_dmg0:                      "boot_div-dmg0",                    Model::DMG0, false,
+    boot_hwio_dmg0:                     "boot_hwio-dmg0",                   Model::DMG0, false,
+//  boot_hwio_dmg0_realbootrom:         "boot_hwio-dmg0",                   Model::DMG0, true,
+    boot_regs_dmg0:                     "boot_regs-dmg0",                   Model::DMG0, false,
+    boot_regs_dmg0_realbootrom:         "boot_regs-dmg0",                   Model::DMG0, true,
+    call_cc_timing:                     "call_cc_timing",                   Model::DMG, false,
+    call_cc_timing2:                    "call_cc_timing2",                  Model::DMG, false,
+    call_timing:                        "call_timing",                      Model::DMG, false,
+    call_timing2:                       "call_timing2",                     Model::DMG, false,
+    div_timing:                         "div_timing",                       Model::DMG, false,
+    ei_sequence:                        "ei_sequence",                      Model::DMG, false,
+    ei_timing:                          "ei_timing",                        Model::DMG, false,
+    halt_ime0_ei:                       "halt_ime0_ei",                     Model::DMG, false,
+    halt_ime0_nointr_timing:            "halt_ime0_nointr_timing",          Model::DMG, false,
+    halt_ime1_timing:                   "halt_ime1_timing",                 Model::DMG, false,
+    if_ie_registers:                    "if_ie_registers",                  Model::DMG, false,
+    intr_timing:                        "intr_timing",                      Model::DMG, false,
+    jp_cc_timing:                       "jp_cc_timing",                     Model::DMG, false,
+    jp_timing:                          "jp_timing",                        Model::DMG, false,
+    ld_hl_sp_e_timing:                  "ld_hl_sp_e_timing",                Model::DMG, false,
+    oam_dma_restart:                    "oam_dma_restart",                  Model::DMG, false,
+    oam_dma_start:                      "oam_dma_start",                    Model::DMG, false,
+    oam_dma_timing:                     "oam_dma_timing",                   Model::DMG, false,
+    pop_timing:                         "pop_timing",                       Model::DMG, false,
+    push_timing:                        "push_timing",                      Model::DMG, false,
+    rapid_di_ei:                        "rapid_di_ei",                      Model::DMG, false,
+    ret_cc_timing:                      "ret_cc_timing",                    Model::DMG, false,
+    ret_timing:                         "ret_timing",                       Model::DMG, false,
+    reti_intr_timing:                   "reti_intr_timing",                 Model::DMG, false,
+    reti_timing:                        "reti_timing",                      Model::DMG, false,
+    rst_timing:                         "rst_timing",                       Model::DMG, false,
 }
 
 // #[test]
