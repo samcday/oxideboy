fac7a81b10f866eb6d32170ff86bcf6e3bda062d
Sam Day <me@samcday.com>
2019-02-27 15:32:37 +0100

    core: give blargg suite the same makeover as mooneye suite using the paste crate

--- oxideboy/tests/blargg.rs
+++ oxideboy/tests/blargg.rs
@@ -63,9 +63,11 @@ fn run_blargg_harness_test(rom: &[u8]) {
 macro_rules! serial_test_cases {
     ( $( $name:ident: $path:tt, )* ) => {
         $(
-        #[test]
-        fn $name () {
-            run_blargg_serial_test(include_bytes!(concat!("blargg/", $path, ".gb")));
+        paste::item! {
+            #[test]
+            fn [<blargg_ $name>] () {
+                run_blargg_serial_test(include_bytes!(concat!("blargg/", $path, ".gb")));
+            }
         }
         )*
     }
@@ -73,9 +75,11 @@ macro_rules! serial_test_cases {
 macro_rules! harness_test_cases {
     ( $( $name:ident: $path:tt, )* ) => {
         $(
-        #[test]
-        fn $name () {
-            run_blargg_harness_test(include_bytes!(concat!("blargg/", $path, ".gb")));
+        paste::item! {
+            #[test]
+            fn [<blargg_ $name>] () {
+                run_blargg_harness_test(include_bytes!(concat!("blargg/", $path, ".gb")));
+            }
         }
         )*
     }
@@ -83,38 +87,39 @@ macro_rules! harness_test_cases {
 
 serial_test_cases! {
     cpu_instrs_01: "cpu_instrs/01-special",
-    blargg_cpu_instrs_02: "cpu_instrs/02-interrupts",
-    blargg_cpu_instrs_03: "cpu_instrs/03-op sp,hl",
-    blargg_cpu_instrs_04: "cpu_instrs/04-op r,imm",
-    blargg_cpu_instrs_05: "cpu_instrs/05-op rp",
-    blargg_cpu_instrs_06: "cpu_instrs/06-ld r,r",
-    blargg_cpu_instrs_07: "cpu_instrs/07-jr,jp,call,ret,rst",
-    blargg_cpu_instrs_08: "cpu_instrs/08-misc instrs",
-    blargg_cpu_instrs_09: "cpu_instrs/09-op r,r",
-    blargg_cpu_instrs_10: "cpu_instrs/10-bit ops",
-    blargg_cpu_instrs_11: "cpu_instrs/11-op a,(hl)",
-    blargg_instr_timing: "instr_timing",
-    blargg_mem_timing: "mem_timing",
+    cpu_instrs_02: "cpu_instrs/02-interrupts",
+    cpu_instrs_03: "cpu_instrs/03-op sp,hl",
+    cpu_instrs_04: "cpu_instrs/04-op r,imm",
+    cpu_instrs_05: "cpu_instrs/05-op rp",
+    cpu_instrs_06: "cpu_instrs/06-ld r,r",
+    cpu_instrs_07: "cpu_instrs/07-jr,jp,call,ret,rst",
+    cpu_instrs_08: "cpu_instrs/08-misc instrs",
+    cpu_instrs_09: "cpu_instrs/09-op r,r",
+    cpu_instrs_10: "cpu_instrs/10-bit ops",
+    cpu_instrs_11: "cpu_instrs/11-op a,(hl)",
+    instr_timing:  "instr_timing",
+    mem_timing:    "mem_timing",
 }
+
 harness_test_cases! {
-    blargg_dmg_sound_01: "dmg_sound/01-registers",
-    blargg_dmg_sound_02: "dmg_sound/02-len ctr",
-    blargg_dmg_sound_03: "dmg_sound/03-trigger",
-    blargg_dmg_sound_04: "dmg_sound/04-sweep",
-    blargg_dmg_sound_05: "dmg_sound/05-sweep details",
-    blargg_dmg_sound_06: "dmg_sound/06-overflow on trigger",
-    blargg_dmg_sound_07: "dmg_sound/07-len sweep period sync",
-    blargg_dmg_sound_08: "dmg_sound/08-len ctr during power",
-    // blargg_dmg_sound_09: "dmg_sound/09-wave read while on",
-    // blargg_dmg_sound_10: "dmg_sound/10-wave trigger while on",
-    blargg_dmg_sound_11: "dmg_sound/11-regs after power",
-    // blargg_dmg_sound_12: "dmg_sound/12-wave write while on",
-    // blargg_oam_bug_01: "oam_bug/1-lcd_sync",
-    // blargg_oam_bug_02: "oam_bug/2-causes",
-    // blargg_oam_bug_03: "oam_bug/3-non_causes",
-    // blargg_oam_bug_04: "oam_bug/4-scanline_timing",
-    // blargg_oam_bug_05: "oam_bug/5-timing_bug",
-    // blargg_oam_bug_06: "oam_bug/6-timing_no_bug",
-    // blargg_oam_bug_07: "oam_bug/7-timing_effect",
-    // blargg_oam_bug_08: "oam_bug/8-instr_effect",
+//  dmg_sound_01:  "dmg_sound/01-registers",
+    dmg_sound_02:  "dmg_sound/02-len ctr",
+    dmg_sound_03:  "dmg_sound/03-trigger",
+    dmg_sound_04:  "dmg_sound/04-sweep",
+    dmg_sound_05:  "dmg_sound/05-sweep details",
+    dmg_sound_06:  "dmg_sound/06-overflow on trigger",
+    dmg_sound_07:  "dmg_sound/07-len sweep period sync",
+    dmg_sound_08:  "dmg_sound/08-len ctr during power",
+//  dmg_sound_09:  "dmg_sound/09-wave read while on",
+//  dmg_sound_10:  "dmg_sound/10-wave trigger while on",
+//  dmg_sound_11:  "dmg_sound/11-regs after power",
+//  dmg_sound_12:  "dmg_sound/12-wave write while on",
+//  oam_bug_01:    "oam_bug/1-lcd_sync",
+//  oam_bug_02:    "oam_bug/2-causes",
+//  oam_bug_03:    "oam_bug/3-non_causes",
+//  oam_bug_04:    "oam_bug/4-scanline_timing",
+//  oam_bug_05:    "oam_bug/5-timing_bug",
+//  oam_bug_06:    "oam_bug/6-timing_no_bug",
+//  oam_bug_07:    "oam_bug/7-timing_effect",
+//  oam_bug_08:    "oam_bug/8-instr_effect",
 }
